3040ac519fb3cdc763a97cb53a5fa3cad8f226fa
kavinda-100 <kavindarathnayake100@gmail.com>
2025-02-25 20:02:25 +0530

    Add database connection settings and implement Stocks and Comments models

---
+++ models/Comments.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FinSharkMarket.models;
+
+public class Comments
+{
+    [Column(TypeName = "uuid")]
+    public Guid Id { get; set; }
+    public String Title { get; set; } = String.Empty;
+    public String Content { get; set; } = String.Empty;
+    // foreign key (stock id)
+    [Column(TypeName = "uuid")]
+    public Guid StockId { get; set; }
+    // navigation property
+    public Stocks? Stock { get; set; }
+    // date fields
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    
+}
---
+++ models/Stocks.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FinSharkMarket.models;
+
+public class Stocks
+{
+    [Column(TypeName = "uuid")]
+    public Guid Id { get; set; }
+    public String Symbol { get; set; } = String.Empty;
+    public String CompanyName { get; set; } = String.Empty;
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Price { get; set; }
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal LastDiv { get; set; }
+    public String Industry { get; set; } = String.Empty;
+    public long MarketCap { get; set; }
+    // relationships
+    public List<Comments> Comments { get; set; } = new List<Comments>();
+    // date fields
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    
+}
