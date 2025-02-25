6460d9f1d9214317b0acc147b72f7860c4fe6fe0
kavinda-100 <kavindarathnayake100@gmail.com>
2025-02-25 21:29:31 +0530

    Add StockController, StockDto, and StockMappers for stock management

---
+++ Controllers/StockController.cs
@@ -0,0 +1,41 @@
+﻿using FinSharkMarket.data;
+using FinSharkMarket.Mappers.stocks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinSharkMarket.Controllers;
+
+[Route("api/stocks")]
+[ApiController]
+public class StockController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+    
+    public StockController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult> GetAllStocks()
+    {
+        var stocks = await _context.Stocks.ToListAsync();
+        // Map Stocks to StockDto
+        var stockDtos = stocks.Select(stock => stock.ToStockDto());
+        
+        return Ok(stockDtos);
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<ActionResult> GetStockById([FromRoute] Guid id)
+    {
+        var stock = await _context.Stocks.FindAsync(id);
+        
+        if (stock == null)
+        {
+            return NotFound("Stock not found");
+        }
+        
+        return Ok(stock.ToStockDto());
+    }
+}
---
+++ Mappers/stocks/StockMappers.cs
@@ -0,0 +1,21 @@
+﻿using FinSharkMarket.Dtos.stocks;
+using FinSharkMarket.models;
+
+namespace FinSharkMarket.Mappers.stocks;
+
+public static class StockMappers
+{
+    // Map Stocks to StockDto (Response body type)
+    public static StockDto ToStockDto(this Stocks stocks)
+    {
+        return new StockDto
+        {
+            Symbol = stocks.Symbol,
+            CompanyName = stocks.CompanyName,
+            Price = stocks.Price,
+            LastDiv = stocks.LastDiv,
+            Industry = stocks.Industry,
+            MarketCap = stocks.MarketCap
+        };
+    }
+}
